da54d0517fe93eac76c67c836ed6a7ed4018a6a5
Andrey Miroshnikov <a.miroshnikov@crpt.ru>
2020-03-18 00:44:32 +0300

    add difference

--- src/main/kotlin/com/github/anmir/notsure/regex.kt
+++ src/main/kotlin/com/github/anmir/notsure/regex.kt
@@ -1,10 +1,42 @@
 package com.github.anmir.notsure
 
+import java.util.regex.Pattern
+
 fun main() {
-    var regex1 = Regex(".*\\s+$")
+//    findSpaces()
+    findWords()
+
+}
+private fun findWords() {
+    var regex1 = Regex(".*[A-Z]+")
+    println("asdA".matches(regex1))
+    println("sdalsddkfj ".matches(regex1))
+    println("sdaldk fj ".matches(regex1))
+    println(" sdaldk fj ".matches(regex1))
+}
+
+private fun findSpaces() {
+    var regex1 = Regex(".*\\s+")
     println("sdaldkfj".matches(regex1))
     println("sdaldkfj ".matches(regex1))
     println("sdaldk fj ".matches(regex1))
     println(" sdaldk fj ".matches(regex1))
+}
+
+
+/**
+ * matches tries to match the expression against the entire string and implicitly add a ^ at the start and $ at the end
+ * of your pattern, meaning it will not look for a substring.
+ * */
+private fun differenceBetweenFindAndMatches() {
+    var p = Pattern.compile("\\d\\d\\d")
+    var m = p.matcher("a123b")
+    println(m.find())
+    println(m.matches())
+
+    p = Pattern.compile("^\\d\\d\\d$")
+    m = p.matcher("123")
+    println(m.find())
+    println(m.matches())
 }
 
